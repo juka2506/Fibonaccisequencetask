cd9d51df872f86a2fd1beb034f01ea28d5570970
Julija Katukova <julija.katukova@gmail.com>
2021-07-05 11:04:22 +0300

    Binet's Formula for the nth Fibonacci number

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the position you want:  ");
+        int position = scanner.nextInt();
+
+        // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987 - Fibonacci numbers
+        // 0, 1, 2, 3, 4, 5, 6, 7,   8,  9, 10, 11,  12,  13,  14,  15,  16 - Position
+
+        //Binet's Formula for the nth Fibonacci number
+        double phi = (Math.sqrt(5) + 1) / 2;
+        double fibOnPosition = (Math.pow(phi, position) - Math.pow((-phi), (-position))) / (Math.sqrt(5));
+
+        System.out.println(position + "th Fibonacci number is " + (int) fibOnPosition);
+    }
+}
